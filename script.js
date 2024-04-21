bab02137dd9853a83aba87219a1123c2541bb32f
Shruthi-10 <150574359+Shruthi-10@users.noreply.github.com>
2024-04-21 08:19:51 -0700

    Third commit

--- script.js
+++ script.js
@@ -21,16 +21,31 @@ addGuestButton.addEventListener("click", function () {
     //  console.log(guest);
 
     if (guest !== "") {
-        const listItem = document.createElement("li");
-        listItem.innerText = guest;
-        guestList.append(listItem);
+        addToList(guest);
+        updateGuestCount();
         clearInput();
     }
-
 });
 
+const clearInput = function() {
+    guestInput.value = "";
+};
 
+const addToList = function (guest) {
+    let listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+};
 
+const updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText = guests.length;
 
-
+    if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+}
+};
 
